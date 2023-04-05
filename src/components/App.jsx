a50df75a9a430ae8d09e9578ff54e0c4996d508a
SpaceVasyl <109691084+SpaceVasyl@users.noreply.github.com>
2023-04-05 20:25:37 +0300

    a

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,26 @@
-import Phonebook from "./PhoneBook/PhoneBook";
-export const App = () => {
-  
-  return (
-    <div
-      style={{
-        height: '100vh',
-        // display: 'flex',
-        // justifyContent: 'center',
-        // alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      <Phonebook/>
-    </div>
-  );
+import  { Component } from "react";
+import Form from "./Form/Form";
+export class App extends Component {
+state = {
+contacts: [],
+name: ''
+}
+handleChange = event => {
+event.preventDefault();
+const asd = event.target.value;
+this.setState({name : asd})
+}
+
+handleSubmit = event => {
+event.preventDefault();
+const contact = this.state.name;
+return contact;
+}
+render() {return (
+  <div>
+    <Form/>
+  </div>
+  )
+}
+
 };
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import css from './PhoneBook.module.css';
 // import { nanoid } from 'nanoid'
-class Phonebook extends Component {
+class Form extends Component {
     constructor(){
         super()
         this.state = {
@@ -9,20 +9,26 @@ class Phonebook extends Component {
             name: ''
           }
     }
-    createNewContact = (e) => {
-      e.preventDefault();
-      const targetClick = e.target[0].value;
-      this.setState(prevState=>{return {name:prevState[targetClick]}});
-      console.log(this.state);
+    handleChange = event => {
+      event.preventDefault();
+    const asd = event.target.value;
+    this.setState({name : asd})
+    }
+    
+    handleSubmit = event => {
+      event.preventDefault();
+      const contact = this.state.name;
+    return contact;
     }
     
     render() {return (<>
     <h2>Phonebook</h2>
-    <form onSubmit={this.createNewContact}>
+    <form onSubmit={this.handleSubmit}>
         <h3>Name</h3>
     <input
         type="text"
         name="name"
+        onChange={this.handleChange}
         className={css.nameinput}
         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
@@ -31,10 +37,9 @@ class Phonebook extends Component {
       />
       <button type="submit">Add contact</button>
       </form>
-      
       <div>
         <h2>Contacts</h2>
-        <ul></ul>
+        <p>{this.handleSubmit}</p>
       </div>
       
       </>
@@ -42,4 +47,4 @@ class Phonebook extends Component {
     }
 }
 
-export default Phonebook;
+export default Form;
